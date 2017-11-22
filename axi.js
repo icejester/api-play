1ca10db29c55645168c0b708158774fca43107ab
Jared R. Mallas <jrmallas@icloud.com>
2017-11-22 11:16:11 -0600

    first axios working...

---
+++ axi.js
@@ -0,0 +1,18 @@
+var axios = require('axios');
+// Performing a GET request
+axios.get('https://api.github.com/users/icejester')
+.then(function(response){
+  console.log(response.data); // ex.: { user: 'Your User'}
+  console.log(response.status); // ex.: 200
+});  
+
+axios.get('https://api.github.com/zen')
+.then(function(response){
+  console.log(response.data);
+  console.log(response.status);
+});
+// Performing a POST request
+//  axios.post('/save', { firstName: 'Marlon', lastName: 'Bernardes' })
+//  .then(function(response){
+//    console.log('saved successfully')
+//  });  
