3cab99733756b361787a8e589361581d5eb233fb
Jared R. Mallas <jrmallas@icloud.com>
2017-11-22 11:39:18 -0600

    nrc still jacked up

--- nrc.js
+++ nrc.js
@@ -2,19 +2,26 @@ var Client = require('node-rest-client').Client;
 
 var client = new Client();
 
-    // do a thing, possibly async, then…
-  
-    if (1) {
-      resolve("Stuff worked!");
-    }
-    else {
-      reject(Error("It broke"));
-    }
-  });
-
-client.get("https://api.github.com/zen", function (data, response) {
-    // parsed response body as js object 
-    console.log(data);
-    // raw response 
-    // console.log(response);
+client.get("https://api.github.com/zen", function (data, response) {})
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log(json);
 });
+
+
+/*
+client.get("https://api.github.com/zen", function (data, response) {
+  // parsed response body as js object 
+  console.log(data);
+  // raw response 
+  // console.log(response);
+});
+
+fetch('https://api.github.com/users/github')
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        console.log(json);
+    });
+*/
